bdd967fb71d76e2a9fd965f2e64324d91a95f301
Miguel Freitas <radiodomiguel70@gmail.com>
2024-05-02 17:33:29 +0100

    Start work on Player application

---
+++ player/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ARView from './ARView/ARView';
+
+function App() {
+  return (
+    <div className="App">
+     <ARView />
+    </div>
+  );
+}
+
+export default App;
