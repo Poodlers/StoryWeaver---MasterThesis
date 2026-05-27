92304e94d6b108538993dc31723cad84c0489ce6
Poodlers <radiodomiguel70@gmail.com>
2026-05-27 10:38:40 +0100

    fix: update editor and player to use proper env vars

--- editor/src/data/constants.tsx
+++ editor/src/data/constants.tsx
@@ -1,6 +1,3 @@
-const BASE_URL = 'https://tese-backend.fly.dev';   // http://localhost:8080, https://tese-backend.fly.dev
+const BASE_URL = process.env.REACT_APP_BACKEND_BASE_URL; // http://localhost:8080, https://tese-backend.fly.dev
 
-
-export {
-    BASE_URL
-}
+export { BASE_URL };
--- player/src/data/constants.tsx
+++ player/src/data/constants.tsx
@@ -1,7 +1,4 @@
-const BASE_URL = 'https://tese-backend.fly.dev';   // http://localhost:8080, https://tese-backend.fly.dev
-const GOOGLE_CLIENT_ID ="256509136029-rcnom0qoutr2uenmvqvf3mu8a5bqqlkq.apps.googleusercontent.com";
+const BASE_URL = process.env.REACT_APP_BACKEND_BASE_URL; // http://localhost:8080, https://tese-backend.fly.dev
+const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
-export {
-    BASE_URL,
-    GOOGLE_CLIENT_ID
-}
+export { BASE_URL, GOOGLE_CLIENT_ID };
